92376eb3ec8c07ffa015de61e2802523565ea624
naf5446 <naf5446@psu.edu>
2026-09-09 12:08:36 -0400

    Update EEG_neonatal.R

--- EEG_neonatal.R
+++ EEG_neonatal.R
@@ -198,3 +198,174 @@ time_elapsed <- system.time({
    }
   
 })
+
+# infant IDs where 1 or 2 experts annotated seizures, expert A
+s12A.IDs <- c(2,8,23,33,54,68)
+
+# infant IDs where 1 or 2 experts annotated seizures, expert B
+s12B.IDs <- c(8,24,64,68,74)
+
+# infant IDs where 1 or 2 experts annotated seizures, expert C
+s12C.IDs <- c(6,12,23,26,33,43,46,54,56,61,64,65,74)
+
+time_elapsed <- system.time({
+  
+   p.ID <- c(s.IDs, s12A.IDs)
+   generate_eeg_waveforms(
+     mode = "seizure",
+     which.expert = we[1],
+     annotations.file = ann.f[1],
+     patient.IDs = p.ID,
+     window = 10,
+     down.sampling.factor = 4,
+     non.seizures.chunks = 10,
+     preprocessing = FALSE,
+     random = FALSE,
+     format = "png",
+     save.to.file = TRUE,
+     dir = dir
+   )
+   
+   p.ID <- c(s.IDs, s12B.IDs)
+   generate_eeg_waveforms(
+     mode = "seizure",
+     which.expert = we[2],
+     annotations.file = ann.f[2],
+     patient.IDs = p.ID,
+     window = 10,
+     down.sampling.factor = 4,
+     non.seizures.chunks = 10,
+     preprocessing = FALSE,
+     random = FALSE,
+     format = "png",
+     save.to.file = TRUE,
+     dir = dir
+   )
+   
+   p.ID <- c(s.IDs, s12C.IDs)
+   generate_eeg_waveforms(
+     mode = "seizure",
+     which.expert = we[3],
+     annotations.file = ann.f[3],
+     patient.IDs = p.ID,
+     window = 10,
+     down.sampling.factor = 4,
+     non.seizures.chunks = 10,
+     preprocessing = FALSE,
+     random = FALSE,
+     format = "png",
+     save.to.file = TRUE,
+     dir = dir
+   )
+  
+})
+
+out = read.csv(
+  '../working/waveforms/expert_A_pat1_64Hz.csv', 
+  sep = "\t", 
+  blank.lines.skip = FALSE
+)
+out[is.na(out)] <- ""
+print(out, row.names = FALSE, na.print = "" , quote = FALSE)
+
+img <- readPNG("../working/waveforms/seizures_expert_A_pat1_no1_win10_len18_from104_to121_64Hz.png")
+grid::grid.raster(img)
+
+range <- c(104, 114)
+patient <- 1
+channel <- 1
+
+out <- generate_selected_waveforms(
+  patient = patient,
+  down.sampling.factor = 4,
+  channel = channel,
+  sec.range = range,
+  preprocessing = FALSE,
+  dir
+)
+
+plot(out$points, type = "l", xlab = "", ylab = "", yaxt = "n", xaxt = "n", main = paste ("Patient #", patient))
+ticks <- seq(1, length(out$points), length.out = 2)
+axis(1, at = ticks, labels = seq(range[1], range[2], length.out = 2), main = "a")
+mtext(text = out$channel.names, las = 1, adj = 0, cex = 1, side = 2, outer = FALSE, line = 3)
+
+out <- read_channel_names(1)
+
+# in EDF file
+out$sig.names.in.edf.file
+
+# after montage
+out$sig.names.after.montage
+
+ann <-
+  read.csv(
+    paste("test_files/sample_annotations.csv", sep = ""),
+    sep = ",",
+    header = TRUE,
+    stringsAsFactors = F,
+    check.names = FALSE,
+    encoding = 'UTF-8'
+  )
+head(ann, 25)
+
+IDs <- c(1, 2)
+
+for (i in IDs) {
+  filename <- paste("test_files/eeg", i, ".edf", sep = "")
+  edf <- read.edf(filename = filename, read.annotations = FALSE, header.only = FALSE)
+  
+  n.sigs <- edf[["header.global"]][["n.signals"]]
+  f.edf <- edf[["header.signal"]][[1]][["n.samples"]]
+  len <- length(edf$signal[[1]]$data)
+  # in seconds
+  len.secs <- len/f.edf
+  
+  sig.names <- NaN
+  for (s in 1:n.sigs) {
+    sig.names[s] <- edf$header.signal[[s]]$label  
+  }
+  sig.names
+  
+  cat("\nEDF file: ", filename,  "\n", sep = "")
+  cat (
+    "  number of signals: ", n.sigs, "\n",
+    "  base frequency: ", f.edf, "\n",
+    "  number of samples: ", len, "\n",
+    "  length in secs: ", len.secs, "\n",
+    sep = "")
+  cat ("  signal names: ", "\n", sep = "")
+  print(sig.names)
+}
+
+for (k in IDs) {
+  filename <- paste(dir, "edf/eeg", k, ".edf", sep = "")
+  cat("EDF file: ", filename,  "\n", sep = "")
+  out <- select_seizure_chunks(data = ann, f.edf, k) 
+  print(out)
+}
+
+filename
+edf[["header.signal"]][["EDF_Annotations"]][["n.samples"]]
+length(edf$signal[["EDF_Annotations"]]$data)
+unique(edf[["signal"]][["EDF_Annotations"]][["data"]])
+plot(edf[["signal"]][["EDF_Annotations"]][["data"]], pch = 16, cex = 0.5, ylab = "")
+
+time_elapsed <- system.time({
+  
+  out <-
+    generate_samples(
+      which.expert = "CC",
+      annotations_file = ann.f[3],
+      seizure.IDs = s12C.IDs,
+      non.seizure.IDs = ns.IDs,
+      window = 10,
+      chunks = 20,
+      down.sampling.factor = 4,
+      preprocessing = FALSE,
+      dir = dir,
+      random = FALSE,
+      write.txt.files = FALSE,
+      write.hdf5.files = TRUE
+    )
+  
+})
